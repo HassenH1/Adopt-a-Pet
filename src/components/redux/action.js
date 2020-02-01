583628a2f18d8dadc2904fd2cb1235c5068ec2c4
HassenH1 <hasansaid51@gmail.com>
2020-01-31 17:10:06 -0800

    haha

--- src/components/redux/action.js
+++ src/components/redux/action.js
@@ -1,10 +1,39 @@
 export const ADD_USER = "ADD_USER"
-export const GET_USER = "GET_USER"
+export const REQUEST_USERDATA = "REQUEST_USERDATA"
+export const GETTING_DATA = "GETTING_DATA"
+export const GETTING_DATA_ERROR = "GETTING_DATA_ERROR"
+
 
 //action creator 
 export const addUser = (user) => {
   return {
     type: ADD_USER,
     payload: user
+  }
+}
+
+export const requestingData = () => {
+  return {
+    type: REQUEST_USERDATA
+  }
+}
+
+export const gettingData = (json) => {
+  return {
+    type: GETTING_DATA,
+    data: json
+  }
+}
+
+export const gettingDataErr = (error) => {
+  return {
+    type: GETTING_DATA_ERROR,
+    data: error
+  }
+}
+
+export const fetchingData = () => {
+  return dispatch => {
+    dispatch()
   }
 }
