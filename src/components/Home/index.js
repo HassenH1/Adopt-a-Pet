b8f72f1060f7b48f2e72779d1aeb35affbeb1328
HassenH1 <hasansaid51@gmail.com>
2020-02-02 21:27:06 -0800

    need another component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { BackgroundColor } from "./styled"
 import Pets from "../Pets"
+import { connect } from 'react-redux'
 
 function Home(props) {
 
   return (
     <BackgroundColor>
-      <Pets />
+      {
+        props.user.username
+          ? ""
+          : <Pets />
+      }
     </BackgroundColor>
   )
 }
 
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
 
-export default Home
+export default connect(mapStateToProps)(Home)
