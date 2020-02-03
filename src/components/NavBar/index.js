9d955a743e2aea27198ca1282455ec70851cba42
HassenH1 <hasansaid51@gmail.com>
2020-02-02 17:51:31 -0800

    icons

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom'
 import { NLink, Div, Links } from './styled'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { Manager, Reference, Popper } from 'react-popper';
 
 function NavBar(props) {
   return (
@@ -15,31 +14,16 @@ function NavBar(props) {
         <div>
           Adopt-A-Pet
         </div>
-        {console.log(props.user, "<----------------from user in Navbar")}
         {
           props.user
             ? <div style={{ width: "90px" }}>
               <Links exact to="/profile">Profile</Links>
               <Links exact to="logout">Logout</Links>
             </div>
-            : <Manager>
-              <Reference>
-                {({ ref }) => (
-                  <div style={{ width: "90px" }}>
-                    <Links exact to="/signup" ref={ref}>SignUp</Links>
-                    <Links exact to="/login" ref={ref}>Login</Links>
-                  </div>
-                )}
-              </Reference>
-              <Popper placement="bottom">
-                {({ ref, style, placement, arrowProps }) => (
-                  <div ref={ref} style={{ width: 120, height: 120, background: '#b4da55' }} data-placement={placement}>
-                    SignUp or Login Here!!
-                    <div ref={arrowProps.ref} style={arrowProps.style} />
-                  </div>
-                )}
-              </Popper>
-            </Manager>
+            : <div style={{ width: "90px" }}>
+              <Links exact to="/signup">SignUp</Links>
+              <Links exact to="/login">Login</Links>
+            </div>
         }
       </Div>
     </NLink>
@@ -52,7 +36,6 @@ const mapStateToProps = (state) => {
   }
 }
 
-// export default withRouter(NavBar)
 export default compose(
   withRouter,
   connect(mapStateToProps)
