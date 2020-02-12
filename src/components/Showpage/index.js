63e4a6794bde93ebca55eedd016a085c3e41d9d8
HassenH1 <hasansaid51@gmail.com>
2020-02-12 15:47:54 -0800

    info

--- src/components/Showpage/index.js
+++ src/components/Showpage/index.js
@@ -23,7 +23,7 @@ function Showpage(props) {
   return (
     <Page>
       <Three>
-          <button onClick={() => props.history.goBack(+1)} >Back</button>
+        <button onClick={() => props.history.goBack(+1)} >Back</button>
       </Three>
       <Outside>
         <One>
@@ -33,7 +33,15 @@ function Showpage(props) {
         </One>
         <Two>
           <h1>Info here</h1>
-          <h3>{props.match.params.id}</h3>
+          <h3>{pet && pet.animal.name}</h3>
+          <h3>{pet && pet.animal.type}</h3>
+          <h3>{pet && pet.animal.breeds.primary}</h3>
+          <h3>{pet && pet.animal.age}</h3>
+          <h3>{pet && pet.animal.gender}</h3>
+          <h3>{pet && pet.animal.description}</h3>
+          <h3>{pet && pet.animal.contact.email}</h3>
+          <h3>{pet && pet.animal.contact.phone}</h3>
+          <h3>{pet && pet.animal.contact.address.address1}</h3>
         </Two>
       </Outside>
     </Page>
@@ -41,3 +49,28 @@ function Showpage(props) {
 }
 
 export default withRouter(Showpage)
+
+// animal:
+// id: 45497377
+// organization_id: "NJ742"
+// url: "https://www.petfinder.com/dog/mickey-hold-45497377/nj/succasunna/cold-nose-warm-heart-dog-rescue-nj742/?referrer_id=c93b6d4c-91f8-4b73-8a19-c73c800d6ecd"
+// type: "Dog"
+// species: "Dog"
+// breeds: {primary: "Labrador Retriever", secondary: "Beagle", mixed: true, unknown: false}
+// colors: {primary: null, secondary: null, tertiary: null}
+// age: "Baby"
+// gender: "Male"
+// size: "Medium"
+// coat: null
+// attributes: {spayed_neutered: false, house_trained: false, declawed: null, special_needs: false, shots_current: true}
+// environment: {children: null, dogs: null, cats: null}
+// tags: []
+// name: "Mickey-HOLD"
+// description: "15 beautiful puppies have arrived into the safety of our foster homes! We are unsure of their breed mix, but..."
+// photos: (5) [{…}, {…}, {…}, {…}, {…}]
+// status: "adopted"
+// status_changed_at: "2019-08-11T19:06:20+0000"
+// published_at: "2019-08-04T16:11:52+0000"
+// distance: null
+// contact: {email: "coldnosewarmheartdogrescue@gmail.com", phone: null, address: {…}}
+// _links: {self: {…}, type: {…}, organization: {…}}
