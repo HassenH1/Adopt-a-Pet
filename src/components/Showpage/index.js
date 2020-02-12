f928863a1b267309f68c4de80be017eff3fa1c27
HassenH1 <hasansaid51@gmail.com>
2020-02-11 20:15:22 -0800

    almost there

--- src/components/Showpage/index.js
+++ src/components/Showpage/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Page, One, Two } from "./styled"
+import { Page, One, Two, Outside } from "./styled"
 
 function Showpage() {
   return (
     <Page>
-      <One>
-        <h1>Image here</h1>
-      </One>
-      <Two>
-        <h1>Info here</h1>
-      </Two>
+      <Outside>
+        <One>
+          <h1>Image here</h1>
+        </One>
+        <Two>
+          <h1>Info here</h1>
+        </Two>
+      </Outside>
     </Page>
   )
 }
--- src/components/Showpage/styled.js
+++ src/components/Showpage/styled.js
@@ -2,21 +2,32 @@ import styled from "styled-components"
 
 export const Page = styled.div`
   height: 100vh;
-  background: whitesmoke;
+  background: #1b99d5;
+  display: flex;
+  justify-content: center;
+`
+
+export const Outside = styled.div`
   display: flex;
   justify-content: space-evenly;
+  width: 85vw;
+  margin: auto;
+  border-radius: 10px;
+  height: 80vh;
+  background: white;
 `
 
 export const One = styled.div`
   width: 30vw;
   border: 1px solid red;
-  height: 80vh;
+  height: 50vh;
   margin: auto 0;
+  text-align: center;
 `
 
 export const Two = styled.div`
   width: 30vw;
   border: 1px solid red;
-  height: 80vh;
+  height: 50vh;
   margin: auto 0;
 `
